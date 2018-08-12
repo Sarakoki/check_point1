d7c01897a9f8ebf7f7604de1c575344b72d1379d
sarakoki <sarakoki94@gmail.com>
2018-08-12 13:33:04 +0300

    start with the front end

--- rest-api/index.js
+++ rest-api/index.js
@@ -1,9 +1,10 @@
 var express = require('express');
 var bodyParser = require('body-parser');
 var Users = require('./models/users');
-
+var path = require('path')
 var app = express();
 app.use(bodyParser.json());
+app.use('/', express.static(path.join(__dirname, 'public')))
 
 
 // Do not touch this invocation of the `listen` method
@@ -11,6 +12,9 @@ app.listen('8888', function () {
   console.log('listening on 8888');
 });
 
+app.get('/',function(err,data) {
+  
+})
 // Do not touch the exports object
 module.exports = app;
 
